43dfc5b7e5b544327885a8a598843f67d22dc512
TurubTermoz471 <115106243+TurubTermoz471@users.noreply.github.com>
2024-10-03 21:49:08 +0700

    Core

---
+++ bash.bashrc
@@ -0,0 +1,20 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+python /data/data/com.termux/files/usr/etc/login_script.py
+
+PS1="[\w] >> "
+
+clear
+echo "Created By Turub471"
+echo "----------------------------" | lolcat
+echo "Instagram :@ Turub471"
+echo "Github: github.com/Turub471/"
+echo "----------------------------" | lolcat
+echo "
++-+-+-+-+-+-+-+
+|W|e|l|c|o|m|e|
++-+-+-+-+-+-+-+ " | lolcat
+date | lolcat
+echo ""
---
+++ login_script.py
@@ -0,0 +1,86 @@
+U
+
+����©Ûý]!��ã�������������������@���s†���d�dl�Z�d�dlmZ�d�dlZdZdZdZdZdZdZd	Z	d
+Z
+dd„�Zd
+d„�Zdd„�Z
+dd„�Zdd„�Zdd„�Zdd„�Zedkr‚eƒ��dS�)é����N)Úgetpassz
+.creed.txtz[1;91mz[1;92mú[1;93mz[1;94mz[1;95mz[1;96mz[1;97mc�������������������C���s���t�j t¡rtƒ��ntƒ��d�S�)N)ÚosÚpathÚisfileÚcreadÚloginÚsingup©�r
+���r
+���úlogin.pyÚsetup���s����r���c���������������	���C���s
+��zPt�ƒ��ttd�ƒ�ttd�ƒ�ttd�ƒ�tdƒ�t d¡�tdƒ�ttd�ƒ�tdƒ�ttdƒƒ}�td	ƒ}tdƒ�ttd
+�ƒ}tdƒ�tt	d�ƒ�tdƒ�tdƒ}|dks¶|r,t
+td
+ƒd}| |�¡�| d¡�| |¡�| d¡�| |¡�| 
+¡��tdƒ�tt	d�ƒ�t d¡�tƒ��W�5�Q�R�X�n$tdƒ�ttd�ƒ�t d¡�tƒ��W�n²�tk
+r¬���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�t d¡�t d¡�Y�nZ�tk
+r���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�t d¡�t d¡�Y�nX�d�S�)Nú 	 Welcome to Termux ú!---------------------------------ú 	 CREATED BY HTR TECH Ú�ú
+date | lolcatr���ú&SET USERNAME,PASSWORD AND RECOVERY KEYz[1;96mUsername [1;92m: z[1;96mPassword [1;92m: úEnter a Recovery key [1;92m: úH[+] Remember your recovery key.Without it you can't change username/passú'[1;94mIf you agree hit Enter [1;94m: ÚwÚ
+úDONE ! Initializing ...ç������à?ú     Wrong Inputçš™™™™™Ù?z9[1;91m     UABLE TO SET THE USERNAME, PASSWORD, RECOVERYç333333Ó?úkillall -9 com.termux)ÚclearÚprintÚgreenÚcyanÚyellowr���ÚsystemÚstrÚinputÚblueÚopenr���ÚwriteÚcloseÚtimeÚsleepÚexitÚredr	���Ú	ExceptionÚKeyboardInterrupt©ZusernameZpasswordZrecoveryÚxÚfr
+���r
+���r���r	������sh����
+
+
+
+
+
+
+
+
+
+
+r	���c���������������	���C���s��zXt�ƒ��ttd�ƒ�ttd�ƒ�ttd�ƒ�tdƒ�t d¡�tdƒ�ttd�ƒ�tdƒ�tttd�ƒƒ}�ttd	�ƒ}tdƒ�ttd
+�ƒ}tdƒ�tt	d�ƒ�tdƒ�tdƒ}|dks¾|r4t
+td
+ƒd}| |�¡�| d¡�| |¡�| d¡�| |¡�| 
+¡��tdƒ�tt	d�ƒ�t d¡�tƒ��W�5�Q�R�X�n$tdƒ�ttd�ƒ�t d¡�tƒ��W�n²�tk
+r´���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�t d¡�t d¡�Y�nZ�tk
+r���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�t d¡�t d¡�Y�nX�d�S�)Nr
+���r���r���r���r���r���r���úUsername [1;92m: zPassword [1;92m: r���r���r���r���r���r���r���r���r���z<[1;91m     UABLE TO CHANGE THE USERNAME, PASSWORD, RECOVERYr���)r���r���r ���r!���r"���r���r#���r$���r%���r&���r'���r���r(���r)���r*���r+���r,���r-���Ú
+singup_oner.���r/���r0���r
+���r
+���r���r4���N���sh����
+
+
+
+
+
+
+
+
+
+
+r4���c���������������	���C���s*��t�ƒ��zxttd�ƒ�ttd�ƒ}�ttdƒ}| ¡�}|d� ¡�}W�5�Q�R�X�|�|krZt	ƒ��n"tdƒ�tt
+d�ƒ�t d¡�W�q�W�q��t
+k
+rÐ���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tt
+d	�ƒ�t d
+¡�Y�q��tk
+r"���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�tt
+d�ƒ�t d
+¡�Y�q�X�q�d�S�)Nz8[+] TO CHANGE THE PASSWORD PLEASE ENTER THE RECOVERY KEYz 
+Enter your Recovery [1;92m: Úré���r���z    Invalid Recovery Keyr���r���z    Wrong Inputr���r���)r���r���r ���r���r!���r'���r���Ú	readlinesÚrstripr4���r-���r*���r+���r.���r/���)ZrecZfiÚdataZrecor
+���r
+���r���Úchange†���s:����
+r:���c���������������	���C���sâ��t�ƒ��ttd�ƒ�ttd�ƒ�ttd�ƒ�tdƒ�t d¡�tdƒ�ttd�ƒ�tdƒ�ttdƒ.}�|� 	¡�}|d	� 
+¡�}|d
+� 
+¡�}|� ¡��W�5�Q�R�X�zžttd�ƒ}tdƒ�t
+td
+�ƒ}||krÀ||krøtdƒ�ttd�|dƒ�t d¡�t�ƒ��W�qÞn:|dks|dkrtƒ��ntdƒ�ttd�ƒ�t d¡�W�q��tk
+rˆ���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�ttd�ƒ�t d¡�Y�q��tk
+rÚ���tdƒ�tdƒ�tdƒ�tdƒ�tdƒ�ttd�ƒ�t d¡�Y�q�X�q�d�S�)Nz 	 Termux Login Script r���r���r���r���z<[+] To change password type "change" in username or passwordz[1;93m r5���r���é���r3���r���zPassword [1;92: z
+ WELCOME ==> z[0mr���r:���r���r���)r���r���r ���r!���r"���r���r#���r'���r���r7���r8���r)���r%���r���r*���r+���r:���r-���r.���r/���)Zflr9���ÚunameZpwordr���Zlogin2r
+���r
+���r���r���¨���s\����
+
+r���c�������������������C���s���t� d¡�d�S�)Nr���©r���r#���r
+���r
+���r
+���r���r���Ü���s����r���c�������������������C���s���t� d¡�d�S�)Nr���r=���r
+���r
+���r
+���r���r,���à���s����r,���Ú__main__)r���r���r*���r���r-���r ���r"���r&���Zpurpler!���Zwhiter���r	���r4���r:���r���r���r,���Ú__name__r
+���r
+���r
+���r���Ú<module>���s&���88"4
